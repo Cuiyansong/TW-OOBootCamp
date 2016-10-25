c9ff12b1be563e699b5faecf8106e8590cb673a0
FOX I <cwpfox@thoughtworks.com>
2016-10-25 14:13:22 +0800

    [cuiyansong] #D1 Init OO BOOT Camp Project and add first code

---
+++ 2016OOBOOTCAMP/UnitTestProject1/OOLength.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    class OOLength
+    {
+        public readonly int value;
+        public readonly string unit;
+
+        public OOLength(int value, string unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var currentValue = 0;
+            var targetValue = 0;
+            var targetObj = ((OOLength)obj);
+
+            if (this.unit == "m")
+            {
+                currentValue = this.value * 100 * 10;
+            }
+            if (this.unit == "cm")
+            {
+                currentValue = this.value * 10;
+            }
+            if (this.unit == "mm")
+            {
+                currentValue = this.value;
+            }
+
+            if (targetObj.unit == "m")
+            {
+                targetValue = targetObj.value * 100 * 10;
+            }
+            if (targetObj.unit == "cm")
+            {
+                targetValue = targetObj.value * 10;
+            }
+            if (targetObj.unit == "mm")
+            {
+                targetValue = targetObj.value;
+            }
+
+            return currentValue == targetValue;
+        }
+
+    }
+}
---
+++ 2016OOBOOTCAMP/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class OOBootCampTest
+    {
+
+        [TestMethod]
+        public void Should_not_euqal_with_length_one_and_two()
+        {
+            OOLength l1 = new OOLength(1, "m");
+            OOLength l2 = new OOLength(2, "m");
+            Assert.AreNotEqual(l1, l2);
+        }
+
+        [TestMethod]
+        public void Should_euqal_with_length_one_and_one()
+        {
+            OOLength l1 = new OOLength(1, "cm");
+            OOLength l2 = new OOLength(1, "cm");
+            Assert.AreEqual(l1, l2);
+        }
+
+        [TestMethod]
+        public void should_equal_compare_1m_with_100cm()
+        {
+            var ooLength1m = new OOLength(1, "m");
+            var ooLength100cm = new OOLength(100, "cm");
+            Assert.AreEqual(ooLength1m, ooLength100cm);
+        }
+
+        [TestMethod]
+        public void should_not_equal_compare_1m_with_1cm()
+        {
+            var ooLength1m = new OOLength(1, "m");
+            var ooLength1cm = new OOLength(1, "cm");
+            Assert.AreNotEqual(ooLength1m, ooLength1cm);
+        }
+    }
+}
